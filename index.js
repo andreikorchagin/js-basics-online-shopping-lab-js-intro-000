0a3f45cbf63b36cd5484dc811fce8e3c87cb3967
Andrei Korchagin <andrei.korchagin@gmail.com>
2018-03-13 18:53:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var cart = [];
 
 function getCart() {
- return cart;
+  return cart;
 }
 
 function setCart(c) {
@@ -10,21 +10,49 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  // write your code here
+  var price = Math.floor(Math.random() * 100) + 1;
+  var newItem = {
+    itemName: item,
+    itemPrice: price
+  };
+  cart.push(newItem);
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
   // write your code here
+
 }
 
 function total() {
   // write your code here
+  var total = 0;
+  for (var i = 0; i < cart.length; i++) {
+    total += cart[i].itemPrice;
+  }
+  return total;
 }
 
 function removeFromCart(item) {
   // write your code here
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].itemName == item) {
+      cart.splice(i, 1);
+      return cart;
+      break;
+    }
+  }
+  return "That item is not in your cart.";
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber) {
+    cart = [];
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+
+  } else {
+    return "Sorry, we don't have a credit card on file for you.";
+  }
 }
