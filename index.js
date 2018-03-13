012e81f5a1358d2cf6064bdf8dcb3a04b4e1fb35
Andrei Korchagin <andrei.korchagin@gmail.com>
2018-03-13 18:55:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,9 +49,9 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if (cardNumber) {
-
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    var totalAmount = totsl();
     cart = [];
+    return `Your total cost is $${totalAmount}, which will be charged to the card ${cardNumber}.`;
 
   } else {
     return "Sorry, we don't have a credit card on file for you.";
