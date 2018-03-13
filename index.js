d7cb80bff26aab6d3a653d61717fed1a1263f93a
Andrei Korchagin <andrei.korchagin@gmail.com>
2018-03-13 18:57:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,14 +22,17 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
-  if(cart.length == 0){
+  if (cart.length == 0) {
     return "Your shopping cart is empty.";
-  }
-  else if(cart.length == 1){
+  } else if (cart.length == 1) {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
-  }
-  else if(cart.length == 2){
+  } else if (cart.length == 2) {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice} and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
+  } else {
+    var cartStr = "In your cart, you have ";
+    for(var i = 0; i<cart.length; i++){
+      
+    }
   }
 }
 
